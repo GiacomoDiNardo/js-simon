3582282968cd84b57e9937f2647c0dc6e2cb45d9
GiacomoDiNardo <vest_95@hotmail.it>
2022-05-31 17:14:58 +0200

    add timer and disappear numbers when 0 sec

--- main.js
+++ main.js
@@ -1,13 +1,57 @@
 const numbersDisplay = document.getElementById("random-numbers");
-const timer = document.getElementById("timer");
+const timerDisplay = document.getElementById("timer");
 
 //creo un array per i numeri casuali
-const randomNumbers = [];
+const randomNumbersContainer = [];
 
 //creo un array per i numeri inseriti dall'utente
 const userNumbers = [];
 
 //genero 5 numeri casuali e li inserisco nell'array
 do{
+    const randomNumber = Math.floor(Math.random() * 100 + 1);
+
+    //esegui push del numero solo se non è già presente
+    if (!randomNumbersContainer.includes(randomNumber)) {
+        randomNumbersContainer.push(randomNumber);
+    }
+} while (randomNumbersContainer.length < 5);
+
+console.log(randomNumbersContainer);
+
+//inserisco i numeri in html
+numbersDisplay.innerText = randomNumbersContainer.join(" - ");
+
+//creo timer di 30 secondi
+
+let timer;
+let minuts = 0;
+let seconds = 10;
+timer = setInterval(function () {
     
-}
+    seconds --;
+
+    if (seconds === 0) {
+        clearInterval(timer);
+        numbersDisplay.classList.add("d-none");
+    }
+
+    console.log("timer " + seconds);
+
+    timerDisplay.innerHTML = 
+    fixStringLength(minuts.toString(), 2, "0") +
+    ":" +
+    fixStringLength(seconds.toString(), 2, "0");
+}, 1000);
+
+
+function fixStringLength(text, length, placeholderChar) {
+    let result = text;
+  
+    while (result.length < length) {
+      result = placeholderChar + result;
+    }
+  
+    return result;
+  }
+
