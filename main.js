05d5a72641414cc5e2e8b702e0a0204760683e51
GiacomoDiNardo <vest_95@hotmail.it>
2022-05-31 16:11:54 +0200

    create repo

---
+++ main.js
@@ -0,0 +1,13 @@
+const numbersDisplay = document.getElementById("random-numbers");
+const timer = document.getElementById("timer");
+
+//creo un array per i numeri casuali
+const randomNumbers = [];
+
+//creo un array per i numeri inseriti dall'utente
+const userNumbers = [];
+
+//genero 5 numeri casuali e li inserisco nell'array
+do{
+    
+}
